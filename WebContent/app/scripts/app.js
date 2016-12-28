1cbbc4f3109b239fe06118a2157d71523165c957
judaondo <judaondo@BM2-161.bilbomatica.es>
2016-12-28 14:28:34 +0100

    ui-router for single page application applied for index.html
    + views folder with header, home and footer created
    + app.js ui.router applied

---
+++ WebContent/app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('moviesApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.html',
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
+                }
+
+            })
+            
+            // route for the contactus page
+            .state('app.insert', {
+                url:'insert',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/insert.html'
+                     }
+                }
+            })
+        
+      
+    
+        $urlRouterProvider.otherwise('/');
+    })
+;
