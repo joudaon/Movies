c54dc695edc66a1811e4003eb2e7656ee6398d91
joudaon <jonuda77@gmail.com>
2017-01-29 17:53:34 +0100

    Changed how genres are stored and displayed.
    + Genres ares stored as an array and displayed as a plain text

--- WebContent/app/scripts/controllers.js
+++ WebContent/app/scripts/controllers.js
@@ -32,7 +32,7 @@ angular.module ('moviesApp')
 		$scope.addMovie = function(){
 			//added "alt" attribute
 			$scope.movie.alt = $scope.movie.title;
-			$scope.movie.genre = $scope.selectedgenres.join(', ');
+			$scope.movie.genre = $scope.selectedgenres;
 			console.log($scope.movie);
 			insertmovieFactory.saveMovie().save({id:$scope.movie.id}, $scope.movie);			
 			growl.success("Movie " + $scope.movie.title + " correctly saved", {title: 'Success', disableCountDown: true});
