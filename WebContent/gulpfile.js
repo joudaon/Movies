e3be53c01996829ef9e6e7b3ef394caf14661ac0
joudaon <jonuda77@gmail.com>
2017-01-30 16:18:51 +0100

    Added jshint support to gulp file

--- WebContent/gulpfile.js
+++ WebContent/gulpfile.js
@@ -1,1 +1,11 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish');
+
+// JSHint
+gulp.task('default', function() {
+	  return gulp.src('app/scripts/**/*.js')
+	  .pipe(jshint())
+	  .pipe(jshint.reporter(stylish));
+	});
+
