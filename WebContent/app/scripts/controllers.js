d1207d04ca4de5f2ae7f690ebbc2a164b4f49a4a
joudaon <jonuda77@gmail.com>
2017-02-02 11:42:55 +0100

    Added genre filters and support on the controller

--- WebContent/app/scripts/controllers.js
+++ WebContent/app/scripts/controllers.js
@@ -51,24 +51,13 @@ angular.module ('moviesApp')
 	.controller ('VideoGalleryController', ['$scope', 'videogalleryFactory', function($scope, videogalleryFactory){				
 		//Get dishes from db_movies.json
 		videogalleryFactory.getMovies().query(
-                function(response) {
-                    $scope.movies = response;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                });	
-		//Adventure filter
-		$scope.filterAdventure = function (){
-			var target_genres2 = ['Adventure'];
-            var movies_array2= $scope.movies;
-            var movies_filtered2 = movies_array2.filter (x => {
-            	return target_genres2.filter (y => {
-            		return x.genre.indexOf (y) >= 0;
-            	}).length;
-            })
-            console.log(movies_filtered2);
-            $scope.movies = movies_filtered2;
-		}
+	        function(response) {
+	            $scope.movies = response;
+	        },
+	        function(response) {
+	            $scope.message = "Error: "+response.status + " " + response.statusText;
+	        });	
+		
 		//Reset filter
 		$scope.reset = function(){
 			videogalleryFactory.getMovies().query(
@@ -77,27 +66,18 @@ angular.module ('moviesApp')
 	                });
 			$scope.moviesfilter = {};
 		}
-		
-/*        //TESTING - Displays movies whose genre is adventure
-        var target_genre = 'Adventure';
-        var movies_array = $scope.movies;
-        var movies_filtered = movies_array.filter ( x => {
-        	return x.genre.indexOf (target_genre) >= 0 
-        });
-        console.log(movies_filtered); 
-        //TESTING - Displays movies whose genre is an aarray -testing purpose
-        var target_genres1 = ['Drama', 'Historical' ];
-        var movies_array1= $scope.movies;
-        var movies_filtered1 = movies_array1.filter (x => {
-        	return target_genres1.filter (y => {
-        		return x.genre.indexOf (y) >= 0;
-        	}).length;
-        })
-        console.log(movies_filtered1);
-        //TESTING - Displays nested genres  testing purpose
-        angular.forEach($scope.movies, function(item){
-        	console.log(item.genre);
-        }) */
+						
+		//Adventure filter
+		$scope.filterAdventure = function (genreButton){
+			//New array with movies with the selected genre passed through parameter
+			var target_genres2 = genreButton;
+            var movies_array2= $scope.movies;
+            var movies_filtered2 = movies_array2.filter ( x => {
+            	return x.genre.indexOf (target_genres2) >= 0 
+            })
+            console.log(movies_filtered2);
+            $scope.movies = movies_filtered2;
+			}
 		
 	}])
 	
@@ -116,6 +96,7 @@ angular.module ('moviesApp')
 			growl.success("Series '" + $scope.serie.title + "' correctly saved", {title: 'Success', disableCountDown: true});
 			$scope.serie = {title:"", year:"", description:""};
 			$scope.seriesform.$setPristine();
-		};			
-	}]);	
+		};
+	}]);
+
 ;
