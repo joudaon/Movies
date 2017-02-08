98541490f9084f82f3b18113f4eb792c01141c08
joudaon <jonuda77@gmail.com>
2017-02-08 11:10:50 +0100

    Gulp added concatenateJS and watch tasks
    + package.json - added required packages for gulp

--- WebContent/gulpfile.js
+++ WebContent/gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
-	jshint = require('gulp-jshint'),
+	jshint = require('gulp-jshint'), 
 	stylish = require('jshint-stylish'),
-	del = require('del');
+	del = require('del'), //Deletes folders
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	babel = require('gulp-babel'), // Fixed lambda expressions
+	usemin = require('gulp-usemin'),
+	rev = require('gulp-rev'), //Gives a hash name to the filename
+	ngAnnotate = require('gulp-ng-annotate'); //Add angularjs dependency injection annotations with ng-annotate
 
 // Default
 gulp.task('default', function(){
@@ -10,12 +16,32 @@ gulp.task('default', function(){
 
 // JSHint - displays javascript file errors
 gulp.task('jshint', function() {
-	  return gulp.src('app/scripts/**/*.js')
-	  .pipe(jshint())
-	  .pipe(jshint.reporter(stylish));
+	return gulp.src('app/scripts/**/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter(stylish));
 	});
 
-//Clean - cleans dist folder
+// Clean - cleans dist folder
 gulp.task('clean', function(){
 	return del(['dist/**']);
 });
+
+// ConcatenateJS - Concatenates all js files into all.js file and uglifies it.
+gulp.task('concatenateJS', function(){
+	return gulp.src('app/scripts/**/*.js')
+	.pipe(ngAnnotate())
+	.pipe(babel({presets: ['es2015']}))
+	.pipe(concat('all.min.js'))
+	.pipe(usemin())
+	.pipe(uglify().on('error', function(e){
+            console.log(e);
+         }))      
+    .pipe(rev())  
+	.pipe(gulp.dest('dist'))
+});
+
+// watchJSfiles - Watches for changes in any js file and executes concatenateJS task
+gulp.task('watchJSfiles', function(){
+	gulp.watch('app/scripts/**/*.js', ['concatenateJS']);
+});
+
